b7593e5faa0eeaa457610a6fa0a130a755332a9f
lixingfeng2016 <lixingfeng2016@163.com>
2020-01-17 16:49:05 +0800

    分类页面基本完成，有bug

---
+++ components/seatchInput/seatchInput.js
@@ -0,0 +1,33 @@
+// components/seatchInput/seatchInput.js
+Component({
+
+  properties:{
+        
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    numbersss: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    headltap(e){
+      console.log('我被点击 了')
+      this.triggerEvent('mytap','哈哈')
+    }
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,152 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    carouselList: [],
+    navigationBars:[],
+    fashions:[],
+    num:'搜索'
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onetwo(e){
+      console.log(e.detail)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.requestCarouselListData();//请求轮播图
+    this.requestnavigationBar();//请求导航栏
+    this.loods();//楼层请求
+  },
+  //请求轮播图
+  requestCarouselListData() {
+    var that = this;//注意this指向性问题
+    // var urlStr = that.data.host + "/api/public/v1/home/swiperdata"; //请求连接注意替换（我用本地服务器模拟）
+    // console.log("请求轮播图：" + urlStr);
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      data: {//这里放请求参数，如果传入参数值不是String，会被转换成String 
+        // x: '',
+        // y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        // console.log("轮播图返回值：");
+        // console.log(res);
+        var resultArr = res.data.message;
+        that.setData({
+          carouselList: resultArr
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+
+  //点击了轮播图
+  chomeCarouselClick: function (event) {
+    var urlStr = event.currentTarget.dataset.url;
+    console.log("点击了轮播图：" + urlStr);
+    // wx.navigateTo({
+    //   url: 'test?id=1'
+    // })
+  },
+
+  // 请求导航栏数据
+  requestnavigationBar(){
+    var that = this;//注意this指向性问题
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      data: {//这里放请求参数，如果传入参数值不是String，会被转换成String 
+        // x: '',
+        // y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        // console.log("轮播图返回值：");
+        // console.log(res);
+        var resultArr = res.data.message;
+        that.setData({
+          navigationBars: resultArr
+        })
+      }
+    })
+  },
+
+  // 楼层请求
+  loods(){
+    var that = this;//注意this指向性问题
+    wx.request({
+      url: "https://api.zbztb.cn/api/public/v1/home/floordata",
+      data: {//这里放请求参数，如果传入参数值不是String，会被转换成String 
+       
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        // console.log("轮播图返回值：");
+        console.log(res);
+        var resultArr = res.data.message;
+        that.setData({
+          fashions: resultArr
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
